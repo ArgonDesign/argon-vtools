7e411f05a39d22dd0751736e192590f118019d99
Geza Lore <gezalore@gmail.com>
2017-06-21 18:12:02 +0100

    Add MANGLEDUNUSED warning

---
+++ src/main/scala/com/argondesign/alint/warnings/MANGLEDUNUSED.scala
@@ -0,0 +1,51 @@
+package com.argondesign.alint.warnings
+
+import com.argondesign.alint.Loc
+import com.argondesign.alint.Source
+import com.argondesign.alint.SourceAnalyser
+import com.argondesign.alint.SourceWarning
+import org.antlr.v4.runtime.ParserRuleContext
+import com.argondesign.alint.antlr4.VLexer
+
+import scala.collection.JavaConverters._
+import org.antlr.v4.runtime.tree.TerminalNode
+
+final case class MANGLEDUNUSED(val loc: Loc, name: String) extends SourceWarning {
+  val message = s"Variable name containing substring 'unused' should have /* verilator lint_on UNUSED */ aplied: '$name'"
+}
+
+object MANGLEDUNUSED extends SourceAnalyser[List[MANGLEDUNUSED]] {
+  def apply(source: Source) = {
+
+    object MANGLEDUNUSEDSourceAnalyserVisitor extends WarningsSourceAnalyserVisitor[MANGLEDUNUSED] {
+      val commentTokenTypes = Set[java.lang.Integer](VLexer.ONE_LINE_COMMENT, VLexer.BLOCK_COMMENT).asJava
+
+      def check(token: TerminalNode) = {
+        val name = token.text
+        if (name.toLowerCase contains "unused") {
+          val precedingComments = source.tokenStream.getTokens(0, token.index, commentTokenTypes) match {
+            case null => Nil
+            case some => some.asScala.toList
+          }
+          val signoff = precedingComments.reverse map (_.text) collectFirst {
+            case s if s matches "/\\*.*verilator.*lint_on.*UNUSED.*\\*/"  => false
+            case s if s matches "/\\*.*verilator.*lint_off.*UNUSED.*\\*/" => true
+          }
+          signoff match {
+            case Some(true) => Nil
+            case _          => List(MANGLEDUNUSED(token.loc, name))
+          }
+        } else {
+          Nil
+        }
+      }
+
+      override def visitVariableTypeUnpacked(ctx: VariableTypeUnpackedContext) = check(ctx.IDENTIFIER)
+      override def visitVariableTypeInitialised(ctx: VariableTypeInitialisedContext) = check(ctx.IDENTIFIER)
+      override def visitNetDeclAssignment(ctx: NetDeclAssignmentContext) = check(ctx.IDENTIFIER)
+      override def visitListOfNetIdentifiers(ctx: ListOfNetIdentifiersContext) = ctx.IDENTIFIER.toList flatMap check
+    }
+
+    MANGLEDUNUSEDSourceAnalyserVisitor(source)
+  }
+}
---
+++ src/test/scala/com/argondesign/alint/warnings/MANGLEDUNUSEDSpec.scala
@@ -0,0 +1,124 @@
+package com.argondesign.alint.warnings
+
+import org.scalatest._
+import com.argondesign.alint.Source
+import com.argondesign.alint.Loc
+
+class MANGLEDUNUSEDSpec extends FlatSpec with Matchers {
+  "MANGLEDUNUSED" should "be detected for variable definition containing 'unused' without a lint_off directive" in {
+    val text = """|module foo;
+                  |  reg aunusedb;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 1
+
+    warnings.head should be(MANGLEDUNUSED(Loc("test.v", 2, 6), "aunusedb"))
+  }
+
+  it should "be detected for variable definition containing 'Unused' without a lint_off directive" in {
+    val text = """|module foo;
+                  |  reg aUnusedb;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 1
+
+    warnings.head should be(MANGLEDUNUSED(Loc("test.v", 2, 6), "aUnusedb"))
+  }
+
+  it should "not be detected for variable definition not containing 'unused'" in {
+    val text = """|module foo;
+                  |  reg used;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 0
+  }
+
+  it should "not be detected for variable definition containing 'unused' with a lint_off directive" in {
+    val text = """|module foo;
+                  |  /* verilator lint_off UNUSED */
+                  |  reg unused;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 0
+  }
+
+  it should "be detected for net definition containing 'unused' without a lint_off directive" in {
+    val text = """|module foo;
+                  |  wire aunusedb;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 1
+
+    warnings.head should be(MANGLEDUNUSED(Loc("test.v", 2, 7), "aunusedb"))
+  }
+
+  it should "be detected for net definition with initializer containing 'unused' without a lint_off directive" in {
+    val text = """|module foo;
+                  |  wire aunusedb = 1'b1;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 1
+
+    warnings.head should be(MANGLEDUNUSED(Loc("test.v", 2, 7), "aunusedb"))
+  }
+
+  it should "be detected for net definition containing 'Unused' without a lint_off directive" in {
+    val text = """|module foo;
+                  |  wire aUnusedb;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 1
+
+    warnings.head should be(MANGLEDUNUSED(Loc("test.v", 2, 7), "aUnusedb"))
+  }
+
+  it should "not be detected for net definition not containing 'unused'" in {
+    val text = """|module foo;
+                  |  wire used;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 0
+  }
+
+  it should "not be detected for net definition containing 'unused' with a lint_off directive" in {
+    val text = """|module foo;
+                  |  /* verilator lint_off UNUSED */
+                  |  wire unused;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 0
+  }
+
+  it should "be detected for net definition containing 'unused' with a lint_on directive" in {
+    val text = """|module foo;
+                  |  /* verilator lint_off UNUSED */
+                  |  /* verilator lint_on UNUSED */
+                  |  wire unused;
+                  |endmodule
+                  |""".stripMargin
+    val warnings = Warnings(MANGLEDUNUSED)(Source("test.v", text))
+
+    warnings should have length 1
+
+    warnings.head should be(MANGLEDUNUSED(Loc("test.v", 4, 7), "unused"))
+  }
+
+}
