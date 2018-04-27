8666c14d77720790dec096ba4f138ac818216093
Geza Lore <gezalore@gmail.com>
2018-04-27 16:22:38 +0100

    Add LICENSE and copyright headers

--- src/main/scala/com/argondesign/alint/Visitor.scala
+++ src/main/scala/com/argondesign/alint/Visitor.scala
@@ -1,3 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////
+// Argon Design Ltd.
+// Copyright (c) 2016-2018 Argon Design Ltd. All rights reserved.
+//
+// This file is covered by the BSD (with attribution) license.
+// See the LICENSE file for the precise wording of the license.
+//
+// Module : Alint
+// Author : Geza Lore
+//
+// DESCRIPTION:
+//
+////////////////////////////////////////////////////////////////////////////////
+
 package com.argondesign.alint
 
 import org.antlr.v4.runtime.tree.RuleNode
